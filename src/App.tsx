2e8c4f9e9403e349fad0c1785cf5e6ff67f9c81b
dickisujadi <dicki.sujadi@eaciit.com>
2022-11-24 08:58:34 +0700

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Sidebar } from './components/Sidebar';
+import { Home } from './pages/Home';
+import { Tags } from './pages/Tags';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-row">
+      <Sidebar />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/tags' element={<Tags />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Tags.tsx
@@ -0,0 +1,5 @@
+export function Tags() {
+    return (
+        <h1>Tags</h1>
+    )
+}
