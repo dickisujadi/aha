6230750039f4484efd55bfc34f6c3f3d0419377e
dickisujadi <dicki.sujadi@eaciit.com>
2022-12-08 13:17:13 +0700

    add broken image handler

--- src/ui/UserCardHome.tsx
+++ src/ui/UserCardHome.tsx
@@ -1,25 +1,27 @@
+import { SyntheticEvent } from "react";
 import { User } from "../typings";
 import ContainedButton from "./ContainedButton";
 import OutlinedButton from "./OutlinedButton";
+import brokenImage from "../assets/broken-image.png";
 
 interface UserCardProps {
     user: User
 }
 
 function UserCard({ user } : UserCardProps) {
+    const brokenImageHandler = (event: SyntheticEvent<HTMLImageElement, Event>) => {
+        event.currentTarget.src = brokenImage;
+    }
+    
     return (
         <div className="w-full mt-[0.1875rem] mb-[0.125rem] mx-0 flex flex-row">
             <div className="w-[70%] h-fit m-0 flex flex-row">
-                <div className="mx-4">
-                    <div className="w-10 h-10 bg-white"></div>
-                    {/* <img 
+                <div className="mx-4 w-10 h-10">
+                    <img 
                         src={`${user.avater}`} 
-                        alt={`${user.id}_${user.username}`} 
-                        onError={({currentTarget}) => {
-                            currentTarget.onerror = null;
-                            currentTarget.src=""
-                        }}
-                    /> */}
+                        onError={brokenImageHandler}
+                        alt={`${user.id}_${user.username}`}
+                    />
                 </div>
                 <div className="flex flex-col">
                     <p className="text-left text-base">{user.name}</p>
