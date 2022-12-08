c53ef1a2da2dcd764e2fad7c5a1f0fd8639fa85a
dickisujadi <dicki.sujadi@eaciit.com>
2022-12-08 12:12:31 +0700

    implement more button functionality

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -15,63 +15,21 @@ interface SearchProps{
 }
 
 export function Search({ onChangePage, keywords, pageSize }: SearchProps) {
-    // const data = [
-    //     {
-    //         title: 'This is a title',
-    //         name: 'by username',
-    //         image: dog
-    //     },
-    //     {
-    //         title: 'This is a title',
-    //         name: 'by username',
-    //         image: deer
-    //     },
-    //     {
-    //         title: 'This is a title',
-    //         name: 'by username',
-    //         image: bay
-    //     },
-    //     {
-    //         title: 'This is a title',
-    //         name: 'by username',
-    //         image: dog
-    //     },
-    //     {
-    //         title: 'This is a title',
-    //         name: 'by username',
-    //         image: deer
-    //     },
-    //     {
-    //         title: 'This is a title',
-    //         name: 'by username',
-    //         image: bay
-    //     },
-    //     {
-    //         title: 'This is a title',
-    //         name: 'by username',
-    //         image: dog
-    //     },
-    //     {
-    //         title: 'This is a title',
-    //         name: 'by username',
-    //         image: deer
-    //     },
-    //     {
-    //         title: 'This is a title',
-    //         name: 'by username',
-    //         image: bay
-    //     },
-    // ]
     const [data, setData] = useState<User[]>([]);
+    const [dataPerPage, setDataPerPage] = useState<number>(pageSize < 9? pageSize : 9);
 
     useEffect(() => {
-        console.log(keywords);
         fetch(`https://avl-frontend-exam.herokuapp.com/api/users/all?page=1&pageSize=${pageSize}&keyword=${keywords}`)
         .then(result => result.json())
         .then((result) => {
             setData(currentData => result.data);
         })
-    }, [])
+    }, []);
+
+    const moreHandler = () => {
+        setDataPerPage(currentNumber => currentNumber + 9)
+    }
+
     return (
         <div className="flex flex-col w-full h-fit mt-[2.125rem]">
             <div className="-ml-[2.375rem] my-6 table">
@@ -80,18 +38,13 @@ export function Search({ onChangePage, keywords, pageSize }: SearchProps) {
                 </div>
                 <h1 className="font-normal text-3xl leading-[2.8125rem] ml-5 text-left">Results</h1>
             </div>
-            <div className="flex flex-row -mx-[.8125rem]">
-                {data && data.map(item => {
-                    // return <div key={item.id} className="flex flex-col w-1/5 h-fit sm:w-1/5 md:w-1/5 lg:w-1/5 xl:w-1/5 2xl:w-1/5">
-                    //     <img src={item.avater} alt="dog" />
-                    //     <h1 className="text-left">This is {item.name}</h1>
-                    //     <h5 className="text-left">by {item.username}</h5>
-                    // </div>
+            <div className="flex flex-row -mx-[.8125rem] flex-wrap">
+                {data && data.slice(0, dataPerPage).map(item => {
                     return <UserCard user={item} key={item.id}/>
                 })}
             </div>
             <div className="w-1/2">
-                <PrimaryButton content="more"/>
+                <PrimaryButton content="more" onButtonClick={moreHandler}/>
             </div>
                 
         </div>
